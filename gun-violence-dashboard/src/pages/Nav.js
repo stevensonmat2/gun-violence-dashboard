d3ef1d81364f64de1098383160bf9609a3976ec3
Matthew Stevenson <31286407+stevensonmat2@users.noreply.github.com>
2023-06-05 15:54:38 -0700

    Update Nav.js

--- gun-violence-dashboard/src/pages/Nav.js
+++ gun-violence-dashboard/src/pages/Nav.js
@@ -8,35 +8,78 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 export const Navbar = () => {
-    return (
-        <><nav className="navbar navbar-expand navbar-light text-align-center bg-primary">
-            <ul className="navbar-nav d-flex flex-wrap">
-                <li className="logo">
-                    <a className="nav-link text-white font-weight-bold" href="/gun-violence-dashboard">Gun Violence Dashboard</a>
-                </li>
-                <li className="nav-item p-2">
-                    <a className="nav-link text-white font-weight-bold" href="/gun-violence-dashboard">Home</a>
-                </li>
-                <li className="nav-item p-2">
-                    <a className="nav-link text-white font-weight-bold" href="/gun-violence-dashboard/explore">Explore</a>
-                </li>
-                <li className="nav-item p-2">
-                    <a className="nav-link text-white font-weight-bold" href="/gun-violence-dashboard/compare">Compare</a>
-                </li>
-                <li className="nav-item p-2">
-                    <a className="nav-link text-white font-weight-bold" href="/gun-violence-dashboard/about">About</a>
-                </li>
-            </ul>
-        </nav>
-            <BrowserRouter>
-                <Routes>
-                    <Route title='Homepage' path='/gun-violence-dashboard' element={<Homepage />} />
-                    <Route title='Explore' path='/gun-violence-dashboard/explore' element={<Explore />} />
-                    <Route title='Compare' path='/gun-violence-dashboard/compare' element={<Compare />} />
-                    <Route title='About' path='/gun-violence-dashboard/about' element={<About />} />
-                </Routes>
-            </BrowserRouter></>
-    );
+  return (
+    <>
+      <nav className="navbar navbar-expand navbar-light text-align-center bg-primary">
+        <ul className="navbar-nav d-flex flex-wrap">
+          <li className="logo">
+            <a
+              className="nav-link text-white font-weight-bold"
+              href="gun-violence-dashboard"
+            >
+              Gun Violence Dashboard
+            </a>
+          </li>
+          <li className="nav-item p-2">
+            <a
+              className="nav-link text-white font-weight-bold"
+              href="gun-violence-dashboard"
+            >
+              Home
+            </a>
+          </li>
+          <li className="nav-item p-2">
+            <a
+              className="nav-link text-white font-weight-bold"
+              href="gun-violence-dashboard/explore"
+            >
+              Explore
+            </a>
+          </li>
+          <li className="nav-item p-2">
+            <a
+              className="nav-link text-white font-weight-bold"
+              href="gun-violence-dashboard/compare"
+            >
+              Compare
+            </a>
+          </li>
+          <li className="nav-item p-2">
+            <a
+              className="nav-link text-white font-weight-bold"
+              href="gun-violence-dashboard/about"
+            >
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            title="Homepage"
+            path="gun-violence-dashboard"
+            element={<Homepage />}
+          />
+          <Route
+            title="Explore"
+            path="gun-violence-dashboard/explore"
+            element={<Explore />}
+          />
+          <Route
+            title="Compare"
+            path="gun-violence-dashboard/compare"
+            element={<Compare />}
+          />
+          <Route
+            title="About"
+            path="gun-violence-dashboard/about"
+            element={<About />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default Navbar;
