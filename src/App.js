c8d63c4d4691ba006748273e9f03501a6fbd63fb
stevensonmat2 <stevensonmat2@gmail.com>
2023-06-05 13:47:05 -0700

    a

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './pages/Nav';
+import React from 'react';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ src/pages/Compare.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// use world ranking where possible
+// "number_of_privately_owned_firearms_-_world_ranking"
+
+function Compare () {
+    return (
+        <h1>This is the Compare Page</h1>
+    );
+}
+
+export default Compare;
